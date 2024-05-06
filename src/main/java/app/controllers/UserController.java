a4e2c1a5933c060a7c69251b2261e1302fabe498
Michella Stuhr Bech <144211387+MichellaBech@users.noreply.github.com>
2024-05-06 13:58:30 +0200

    sql

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,4 +1,35 @@
 package app.controllers;
 
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class UserController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("/contactinfo.html",ctx -> createuser(ctx, connectionPool));
+    }
+
+    private static void createuser(Context ctx, ConnectionPool connectionPool)
+    {
+        //hent form parameter
+        String fullname = ctx.formParam("fullname");
+        String adress = ctx.formParam("adress");
+        Integer phonenumber = Integer.valueOf(ctx.formParam("phonenumber"));
+        Integer zipcode = Integer.valueOf(ctx.formParam("zipcode"));
+        String email = ctx.formParam("email");
+
+        try {
+            UserMapper.createuser(fullname, adress, phonenumber, zipcode, email, connectionPool);
+
+        } catch (DatabaseException e) {
+            ctx.attribute("message", "Der skete en fejl, prøv igen");
+            ctx.render("createuser.html");
+        }
+
+    }
+
 }
+
